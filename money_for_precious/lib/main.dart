2166d8acb56eff22856436bb39064c4d7d877ccc
toma-0 <toma0122yukako@gmail.com>
2022-11-18 05:53:48 +0900

    イメージ追加と大まかなホーム画面の色を作成

--- money_for_precious/lib/main.dart
+++ money_for_precious/lib/main.dart
@@ -1,2 +1,50 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
+//残高と推しの名前、ユーザーネーム、アイコン等を表示
+
+void main() => runApp(
+      MaterialApp(
+        home: HomePage(),
+      ),
+    );
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+      body: ATM(),
+    );
+  }
+}
+
+class ATM extends StatefulWidget {
+  @override
+  _ATMState createState() => _ATMState();
+}
+
+class _ATMState extends State<ATM> {
+  int ATMNumber = 0;
+
+  void OpenATM() {
+    setState(() {
+      ATMNumber = 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+          onPressed: () {
+            OpenATM();
+          },
+          child: Image.asset("images/$ATMNumber.png")),
+    );
+  }
+}
